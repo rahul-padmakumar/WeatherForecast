99a1fd0e010fc3740eb11a829decb0a50dc98d06
rahulp <rahulpadmakumar>
2023-05-29 09:38:48 +0530

    Add navigation and show splash screen.

---
+++ app/src/main/java/com/example/weatherforecast/navigation/Screens.kt
@@ -0,0 +1,9 @@
+package com.example.weatherforecast.navigation
+
+enum class Screens {
+    SPLASH_SCREEN,
+    MAIN_SCREEN,
+    ABOUT_SCREEN,
+    FAVORITE_SCREEN,
+    SETTINGS_SCREEN
+}
---
+++ app/src/main/java/com/example/weatherforecast/screens/WeatherSplashScreen.kt
@@ -0,0 +1,10 @@
+package com.example.weatherforecast.screens
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun WeatherSplashScreen(navController: NavController){
+    Text(text = "Splash Screen")
+}
